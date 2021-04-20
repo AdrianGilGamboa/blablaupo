14bd503591a1f759d225a40763c8ffa6d9da9c92
mamofu <63147643+mamofu@users.noreply.github.com>
2021-04-20 20:24:50 +0200

    Update registroAction.java

--- blablaupo/src/java/action/usuario/registroAction.java
+++ blablaupo/src/java/action/usuario/registroAction.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package action;
+package action.usuario;
 
 import com.opensymphony.xwork2.ActionSupport;
 import entidades.Usuarios;
