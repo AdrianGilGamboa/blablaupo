ccae46ff1cb579b9d6b34a864259de2ecfeb000c
mamofu <63147643+mamofu@users.noreply.github.com>
2021-04-20 20:43:07 +0200

    Update registroAction.java

--- blablaupo/src/java/action/usuario/registroAction.java
+++ blablaupo/src/java/action/usuario/registroAction.java
@@ -22,11 +22,82 @@ public class registroAction extends ActionSupport {
      private String email;
      private String password;
      private String tipo;
+
+    public Usuarios getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuarios usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+     
+     
     public registroAction() {
     }
     
     public String execute() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        usuario.setEmail(getEmail());
+        usuario.setDni(getDni());
+        usuario.setNombre(getNombre());
+        usuario.setTelefono(getTelefono());
+        usuario.setApellidos(getApellidos());
+        usuario.setPassword(getPassword());
     }
     
 }
